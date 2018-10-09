632e37b3d0adf936b53013f5bdcf7a869973071f
Mario Hoyos <mariohoyos92@gmail.com>
2018-10-09 12:31:45 -0500

    feat: add demo script

--- server/index.js
+++ server/index.js
@@ -3,17 +3,21 @@ const express = require("express");
 const { json } = require("body-parser");
 
 const app = express();
+
+// SERVE FRONTEND
+app.use(express.static(`${__dirname}/../build/`));
+
 app.use(json());
 
 const creditCard = {
-  balance: 1200
+    balance: 1200
 };
 
 app.use("/balance", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    balance: creditCard.balance
-  });
+    res.status(200).json({
+        status: "success",
+        balance: creditCard.balance
+    });
 });
 
 const port = process.env.PORT || 3001;
